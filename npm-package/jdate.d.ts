c039f4a84e00d80bd130b809b457c1ef973b1438
Joel Grayson <joel@joelgrayson.com>
2023-08-21 08:23:40 -0400

    updated jdate to include shortYear optional parameter

--- npm-package/jdate.d.ts
+++ npm-package/jdate.d.ts
@@ -1,4 +1,4 @@
 /**
- * Returns a date in the format \<month\>.\<day\>.\<year\>. If no date is passed it, in uses today as the date.
+ * Returns a date in the format \<month\>.\<day\>.\<year\>. If no date is passed it, in uses today as the date. Short year means showing 22 instead of 2022
  */
-export default function jdate(date?: Date): string;
+export default function jdate(date?: Date, shortYear?: boolean): string;
--- npm-package/jdate.js
+++ npm-package/jdate.js
@@ -1,6 +1,6 @@
-module.exports=function jdate(date=new Date()) {
+module.exports=function jdate(date=new Date(), shortYear=false) {
     const month=date.getMonth()+1;
     const day=date.getDate();
     const year=date.getFullYear();
-    return `${month}.${day}.${year}`;
+    return `${month}.${day}.${shortYear ? year.toString().slice(2) : year}`;
 }
