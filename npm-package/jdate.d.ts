f692f8fdc88e9da4b57e285e9e3962d52ea3030f
Joel Grayson <joel@joelgrayson.com>
2024-01-16 10:58:29 -0500

    npm package joeldate has toDate(), isJdate(), and the option to make jdate() typesafe
    When typesafe, jdate() returns a date if it gets one

--- npm-package/jdate.d.ts
+++ npm-package/jdate.d.ts
@@ -1,4 +1,8 @@
 /**
  * Returns a date in the format \<month\>.\<day\>.\<year\>. If no date is passed it, in uses today as the date. Short year means showing 22 instead of 2022
  */
-export default function jdate(date?: Date, shortYear?: boolean): string;
+type Jdate=string;
+export default function jdate(date?: Date, shortYear?: boolean, typesafe?: boolean): Jdate;
+export function jdate(date?: Date, shortYear?: boolean, typesafe?: boolean): Jdate;
+export function toDate(jdate: Jdate | any): Date | null;
+export function isJdate(item: Jdate | any): boolean;
--- npm-package/jdate.js
+++ npm-package/jdate.js
@@ -1,6 +1,51 @@
-module.exports=function jdate(date=new Date(), shortYear=false) {
+function jdate(date=new Date(), shortYear=false, typesafe=false) {
+    if (typesafe && isJdate(date))
+        return date;
+    
     const month=date.getMonth()+1;
     const day=date.getDate();
     const year=date.getFullYear();
     return `${month}.${day}.${shortYear ? year.toString().slice(2) : year}`;
 }
+
+function isJdate(item) { //item: any -> boolean
+    item=item.trim();
+    if (typeof item!=='string')
+        return false;
+    
+    return /^\d{1,2}\.\d{1,2}\.\d{2,4}$/.test(item);
+}
+
+function toDate(jdate) {
+    if (jdate instanceof Date)
+        return jdate;
+    if (typeof jdate==='string') {
+        jdate=jdate.trim();
+        if (!isJdate(jdate))
+            return null;
+        
+        let [month, day, year]=jdate.split('.');
+        month--;
+        if (year.length===2)
+            year='20'+year;
+        
+        let date=new Date();
+        date.setMonth(month);
+        date.setDate(day);
+        date.setFullYear(year);
+        return date;
+    }
+
+    const lastAttempt=new Date(jdate);
+    if (isNaN(lastAttempt))
+        return null;
+    else
+        return lastAttempt;
+}
+
+module.exports={
+    default: jdate,
+    jdate,
+    isJdate,
+    toDate
+};
